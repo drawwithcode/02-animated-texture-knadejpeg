4a71056597323a4f3fd8a64bbf3c15834b84e7f3
knadejpeg <mercedesmaria.vitali@mail.polimi.it>
2021-10-13 08:22:14 +0200

    first upload
    v1

--- sketch.js
+++ sketch.js
@@ -1,31 +1,30 @@
 function setup() {
- createCanvas(windowWidth, windowHeight);
- background(0);
  frameRate(32);
-
- // text
-fill (255);
-
-text('look for straight lines :)', 30, 30);
-
-}
-
- function draw() {
-
- for (let x = 0; x < windowWidth; x += 5) {
-  for (let y = 0; y < windowHeight; y += 5) {
-
-   // the effect can be controlled moving around
-   let i = mouseX; // x increment
-   let t = mouseY; // y increment
-   // define noise index with increment
-   let n = noise (x * 0.005 * i , y * 0.01 * t);
-   // relate noise and color
-   fill (n*255, n*0, n*255);
-
-   noStroke();
-   square(x, y, 2);
-  }
- }
-
-}
+    createCanvas(windowWidth, windowHeight);
+    background(0);
+    frameRate(32);
+   // text
+   fill (255);
+   text('look for straight lines :)', 30, 30);
+   }
+   
+   
+function draw() {
+   
+    for (let x = 0; x < windowWidth; x += 5) {
+     for (let y = 0; y < windowHeight; y += 5) {
+   
+      // the effect can be controlled moving around
+      let i = mouseX; // x increment
+      let t = mouseY; // y increment
+      // define noise index with increment
+      let n = noise (x * 0.005 * i , y * 0.01 * t);
+      // relate noise and color
+      fill (n*255, n*0, n*255);
+   
+      noStroke();
+      square(x, y, 2);
+     }
+    }
+   
+   }
